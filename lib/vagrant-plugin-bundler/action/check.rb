a02e2fa14bf110c7703c3f1e1bc6ebcfe7eff053
tkn <tkn@zuehlke.com>
2013-06-13 00:57:22 +0200

    extract installed_plugins to method

--- lib/vagrant-plugin-bundler/action/check.rb
+++ lib/vagrant-plugin-bundler/action/check.rb
@@ -6,30 +6,20 @@ module VagrantPlugins
       class Check
 
         def initialize(app, env)
-          @app    = app
-          @installed_plugins = {}
+          @app = app
         end
 
         def call(env)
           
-          # check plugins
-          #output = `D:/Repos/_github/bills-kitchen/target/build/tools/vagrant/HashiCorp/Vagrant/bin/vagrant plugin list`
-          output = `vagrant plugin list`
-          
-          output.each_line do |line|
-            name, version = line.match(/\s*(\S+)\s*\((\S+)\)/).captures
-            @installed_plugins[name]=version
-          end
-
           required_plugins = env[:machine].config.plugin.dependencies
           
-          missing_plugins,good_plugins = required_plugins.partition do |plugin,version|
-            required_plugins[plugin] != @installed_plugins[plugin]
+          missing_plugins, good_plugins = required_plugins.partition do |plugin, version|
+            required_plugins[plugin] != installed_plugins[plugin]
           end
           
           unless missing_plugins.empty?
             errors = missing_plugins.map do |plugin, required_version| 
-              installed_version = @installed_plugins[plugin]
+              installed_version = installed_plugins[plugin]
               if installed_version
                 I18n.t('vagrant_plugin_bundler.errors.plugin_version_error', 
                   :plugin => plugin,
@@ -46,7 +36,17 @@ module VagrantPlugins
 
           # continue if ok
           @app.call(env)
+        end
 
+        def installed_plugins
+          unless @installed_plugins
+            #`D:/Repos/_github/bills-kitchen/target/build/tools/vagrant/HashiCorp/Vagrant/bin/vagrant plugin list`
+            `vagrant plugin list`.each_line do |line|
+              name, version = line.match(/\s*(\S+)\s*\((\S+)\)/).captures
+              @installed_plugins[name] = version
+            end
+          end
+          @installed_plugins
         end
       end
     end
