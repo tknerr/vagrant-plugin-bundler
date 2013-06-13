241db3bb7d2993caad299c2a5160570048dcd200
tkn <tkn@zuehlke.com>
2013-06-13 02:36:50 +0200

    refactor: remove redundant nesting level in specs

--- spec/vagrant-plugin-bundler/check_spec.rb
+++ spec/vagrant-plugin-bundler/check_spec.rb
@@ -10,42 +10,39 @@ describe VagrantPlugins::PluginBundler::Action::Check do
 
   I18n.load_path << File.expand_path("../../../locales/en.yml", __FILE__)
   I18n.reload!
-
-  describe "checking dependencies" do
-    
-    context "when matching" do
-      before do
-        check.stub(:installed_plugins => {'foo' => '1.2.3'})
-        check.stub(:required_plugins => {'foo' => '1.2.3'})
-      end
-      it "should proceed" do
-        app.should_receive(:call)
-        check.call(env)
-      end
+  
+  context "when matching" do
+    before do
+      check.stub(:installed_plugins => {'foo' => '1.2.3'})
+      check.stub(:required_plugins => {'foo' => '1.2.3'})
     end
-    
-    context "when wrong version" do
-      before do
-        check.stub(:installed_plugins => {'foo' => '1.2.3'})
-        check.stub(:required_plugins => {'foo' => '1.2.4'})
-      end
-      it "should error with wrong version" do
-        expect { check.call(env) }.to raise_error(
-          VagrantPlugins::PluginBundler::Errors::PluginsNotFoundError,
-          /Version 1.2.4 of foo required. Version 1.2.3 installed/)
-      end
+    it "should proceed" do
+      app.should_receive(:call)
+      check.call(env)
     end
+  end
+  
+  context "when wrong version" do
+    before do
+      check.stub(:installed_plugins => {'foo' => '1.2.3'})
+      check.stub(:required_plugins => {'foo' => '1.2.4'})
+    end
+    it "should error with wrong version" do
+      expect { check.call(env) }.to raise_error(
+        VagrantPlugins::PluginBundler::Errors::PluginsNotFoundError,
+        /Version 1.2.4 of foo required. Version 1.2.3 installed/)
+    end
+  end
 
-    context "when plugin not installed" do
-      before do
-        check.stub(:installed_plugins => {'bar' => '1.2.3'})
-        check.stub(:required_plugins => {'foo' => '1.2.3'})
-      end
-      it "should error with plugin not installed" do
-        expect { check.call(env) }.to raise_error(
-          VagrantPlugins::PluginBundler::Errors::PluginsNotFoundError,
-          /Version 1.2.3 of foo required. Plugin not installed/)
-      end
+  context "when plugin not installed" do
+    before do
+      check.stub(:installed_plugins => {'bar' => '1.2.3'})
+      check.stub(:required_plugins => {'foo' => '1.2.3'})
+    end
+    it "should error with plugin not installed" do
+      expect { check.call(env) }.to raise_error(
+        VagrantPlugins::PluginBundler::Errors::PluginsNotFoundError,
+        /Version 1.2.3 of foo required. Plugin not installed/)
     end
   end
 end
